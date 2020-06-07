67aba884f9d0b5917bea3e3033acd592428f8420
BravoMJE <bravom.j.e@gmail.com>
2020-06-06 19:53:38 -0500

    Creacion del proyecto

---
+++ Practica0AD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practica0AD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
